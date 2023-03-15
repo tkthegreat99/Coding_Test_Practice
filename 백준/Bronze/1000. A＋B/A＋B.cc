9f3ecc1f35844f6901a6d707ebf8fa4941d25014
Taegang Yun <73454135+tkthegreat99@users.noreply.github.com>
2023-03-15 20:14:48 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string str;
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << '\n';
+    
+    
+    str = "SAL";
+}
