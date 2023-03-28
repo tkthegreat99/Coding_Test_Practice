a6c264a37ddea9cea071fa1cfbca5f4f0469df3f
Taegang Yun <73454135+tkthegreat99@users.noreply.github.com>
2023-03-28 12:11:08 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <map>
+#include <stack>
+
+
+using namespace std;
+
+
+typedef long long ll;
+
+ll a, b, c;
+ll go(ll a, ll b) {
+
+	if (b == 1) return a % c;
+
+	ll ret = go(a, b / 2);
+	ret = (ret * ret) % c;
+	if (b % 2) ret = (ret * a) % c;
+
+
+	return ret;
+}
+
+
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+		
+
+	cin >> a >> b >> c;
+	cout << go(a, b) << '\n';
+
+	return 0;
+}
+
+	
+
