d22120d80d13b6816ca339ebcda7bacedb87402c
Taegang Yun <73454135+tkthegreat99@users.noreply.github.com>
2023-04-02 20:06:04 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -26,7 +26,7 @@ void dfs(int y, int x) {
 	for (int i = 0; i < 4; i++) {
 		int ny = y + dy[i];
 		int nx = x + dx[i];
-		if (ny < 0 || ny >= n || nx < 0 || nx >= m || a[ny][nx] == 0) continue;
+		if (ny < 0 || ny >= n || nx < 0 || nx >= m ) continue;
 		if (a[ny][nx] == 1&& !visited[ny][nx]) dfs(ny, nx);
 	}
 	return;
