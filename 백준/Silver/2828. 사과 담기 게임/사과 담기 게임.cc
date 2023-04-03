f57f3e6816bc790d58c154211470772fde789bc8
Taegang Yun <73454135+tkthegreat99@users.noreply.github.com>
2023-04-03 21:44:15 +0900

    [Silver V] Title: 사과 담기 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int n, m, j, l, r, tmp, ret;
+
+int main() {
+	cin >> n >> m >> j;
+	l = 1;
+	for (int i = 0; i < j; i++) {
+		r = l + m - 1;
+		cin >> tmp;
+		if (tmp >= l && tmp <= r) continue;
+		else {
+			if (tmp < l) {
+				ret += l - tmp;
+				l = tmp;
+			}
+			else {
+
+				l += tmp - r;
+				ret += tmp - r;
+
+			}
+		}
+	}
+
+
+	cout << ret;
+
+}
