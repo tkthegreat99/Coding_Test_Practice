febe8fba1da576c00bfb743d362f298e095f9171
Taegang Yun <73454135+tkthegreat99@users.noreply.github.com>
2024-03-12 14:17:20 +0900

    [level 2] Title: 피보나치 수, Time: 0.90 ms, Memory: 3.94 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+vector<int> fibona;
+
+int fibo(int n){
+    if(n == 0) return 0;
+    if(n == 1) return 1;
+    for(int i = 2; i <= n; i++){
+        fibona.push_back(fibona[i-1] % 1234567 + fibona[i-2] % 1234567);
+    }
+    return fibona[n];
+}
+
+
+int solution(int n) {
+    fibona.push_back(0);
+    fibona.push_back(1);
+    return fibo(n) % 1234567;
+}
