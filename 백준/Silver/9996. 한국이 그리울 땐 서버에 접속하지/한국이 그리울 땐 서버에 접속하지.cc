cf92d76d1f8807261b330b87c32bb06ed7a561f6
Taegang Yun <73454135+tkthegreat99@users.noreply.github.com>
2024-06-27 13:42:43 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -1,58 +1,44 @@
 #include <iostream>
+#define fastio cout.tie(0), cin.tie(0), ios_base::sync_with_stdio(0)
 #include <vector>
 #include <algorithm>
 #include <string>
-#include <queue>
+#include <map>
 
 using namespace std;
 
-vector<string> split(string input, string deli) {
-	string token = "";
-	vector<string> ret;
-	long long pos = 0;
-	while ((pos = input.find(deli)) != string::npos) {
-		token = input.substr(0, pos);
-		ret.push_back(token);
-		input.erase(0, pos + deli.length());
-	}
-	ret.push_back(input);
-	return ret;
-}
-
+int n;
+string ptn, str;
 
 int main() {
-
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr); cout.tie(nullptr);
-
-	int n;
-	cin >> n;  // n개의 줄
+	fastio;
 	
+	cin >> n;
 
-	vector<string> v; //저장할 벡터
-	string str; //입력받을 패턴
-	cin >> str;
-	
-	v = split(str, "*");
+	cin >> ptn;
 
+	auto it = ptn.find("*");
+	string pt1 = ptn.substr(0, it);
+	string pt2 = ptn.substr(it + 1);
 
-	string input;
+	int chk = 1;
 	
 	for (int i = 0; i < n; i++) {
 
-		cin >> input;
-		if (v[0].length() + v[1].length() > input.length()) cout << "NE" << '\n';
+		cin >> str;
+
+		if (str.length() < pt1.length() + pt2.length()) chk = 0;
 		else {
-			if (v[0] == input.substr(0, v[0].length()) && v[1] == input.substr(input.length() - v[1].length())) cout << "DA" << '\n';
-			else cout << "NE" << '\n';
+
+			if (str.substr(0, pt1.length()) == pt1 && str.substr(str.length() - pt2.length()) == pt2) chk = 1;
+			else chk = 0;
 		}
+
+		if (chk) cout << "DA" << '\n';
+		else cout << "NE" << '\n';
 
 	}
 
-	
+
 	return 0;
-	
 }
-
-	
-
