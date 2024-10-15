52f42d84cf3e183ff0b4e2b98ab7e6d2e04bf583
Taegang Yun <73454135+tkthegreat99@users.noreply.github.com>
2024-10-15 19:44:45 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.27 ms, Memory: 4.06 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -5,42 +5,39 @@
 #include <queue>
 using namespace std;
 
-int visited[104][104];
-int a[104][104];
 const int dy[4] = {-1, 0, 1, 0};
 const int dx[4] = {0, 1, 0, -1};
-int mn;
-int x, y;
+int visited[101][101];
+
+int y, x;
 
 int solution(vector<vector<int> > maps)
 {
     int answer = 0;
+    
     int n = maps.size();
     int m = maps[0].size();
-    for(int i = 0; i < n; i++){
-        for(int j = 0 ; j < m; j++){
-            a[i][j] = maps[i][j];
-        }
-    }
-    fill(&visited[0][0] , &visited[0][0] + 104 * 104, -1);
-  
     
     queue<pair<int, int>> q;
     visited[0][0] = 1;
     q.push({0, 0});
-    while(q.size()){
+    while(q.size())
+    {
         tie(y, x) = q.front(); q.pop();
         for(int i = 0 ; i < 4; i++){
             int ny = y + dy[i];
             int nx = x + dx[i];
-            if(ny < 0 || ny >=n || nx < 0 || nx >= m || a[ny][nx] == 0) continue;
-            if(visited[ny][nx] > -1) continue;
+            if(ny < 0 || nx < 0 || nx >= m || ny >= n) continue;
+            if(visited[ny][nx] || !maps[ny][nx]) continue;
             visited[ny][nx] = visited[y][x] + 1;
             q.push({ny, nx});
         }
     }
-
-    answer = visited[n-1][m-1];
+    
+    if(visited[n-1][m-1] == 0) return -1;
+    
+    
+    answer = visited[n - 1][m - 1];
     
     return answer;
 }
