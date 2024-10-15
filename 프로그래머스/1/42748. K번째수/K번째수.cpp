854da5cc3c80af6265c8c7fd5f9847f56840381a
Taegang Yun <73454135+tkthegreat99@users.noreply.github.com>
2024-10-15 18:26:49 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -3,24 +3,23 @@
 #include <algorithm>
 #include <iostream>
 
+
 using namespace std;
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
-    vector<int> answer;
+    vector<int> answer;   
     
-    vector<int> tmp;
-    
-    for(int i = 0 ; i < commands.size(); i++){
-        for(int j = commands[i][0] - 1 ; j < commands[i][1]; j++){
+    for(int i = 0 ; i < commands.size(); i++)
+    {
+        vector<int> tmp; //임시 정수 벡터 선언
+        for(int j = commands[i][0] - 1; j < commands[i][1]; j++)
+        {
+            
             tmp.push_back(array[j]);
         }
-        /*
-        cout << "tmp : ";
-        for(int x : tmp) cout << x  << " ";
-        */
         sort(tmp.begin(), tmp.end());
         answer.push_back(tmp[commands[i][2] - 1]);
-        tmp.clear();
     }
+    
     return answer;
 }
