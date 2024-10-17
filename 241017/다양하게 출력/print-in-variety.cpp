279317eba5b94348fffe9c26f23a8d7610caf74e
Taegang Yun <73454135+tkthegreat99@users.noreply.github.com>
2024-10-17 23:49:22 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241017/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Total days in Year\n";
+    cout << "365\n";
+    cout << "Circumference rate\n";
+    cout << "3.1415926535";
+
+
+    return 0;
+}
