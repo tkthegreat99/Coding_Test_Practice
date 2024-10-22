988b9b49590dbdffa096e885e4ceba2267fd6dc6
Taegang Yun <73454135+tkthegreat99@users.noreply.github.com>
2024-10-22 11:59:38 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.03 ms, Memory: 3.87 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.cpp
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.cpp
@@ -5,45 +5,83 @@
 
 using namespace std;
 
-int time(string str){
-   return ((str[0] - '0') * 10 + (str[1] - '0')) * 60 + (str[3] - '0') * 10 + str[4] - '0';
+int time(string str)
+{
+    return ((str[0] - '0') * 10 + (str[1] - '0')) * 60 + (str[3] - '0') * 10 + str[4] - '0';
 }
 
+string timetostr(int time)
+{
+    string tmp = "";
+    int hour = time / 60;
+    if(hour < 10)
+    {
+        tmp += "0";
+        tmp += to_string(hour);
+    }
+    else tmp += to_string(hour);
+    
+    tmp += ":";
+    int min = time % 60;
+    if(min < 10)
+    {
+        tmp += "0";
+        tmp += to_string(min);    
+    }
+    else tmp += to_string(min);
+    return tmp;
+}
 
+int prev(int curtime)
+{
+    if(curtime < 10) curtime = 0;
+    else curtime -= 10;
+    return curtime;
+}
+
+int next(int curtime, int videolen)
+{
+    if(videolen - curtime < 10)
+    {
+        curtime = videolen;
+    }
+    else curtime += 10;
+    return curtime;
+}
+
+int pass(int curtime, int op_start, int op_end)
+{
+    if(curtime >= op_start && curtime <= op_end)
+    {
+        curtime = op_end;
+    }
+    return curtime;
+}
 
 string solution(string video_len, string pos, string op_start, string op_end, vector<string> commands) {
     string answer = "";
-    int ps = time(pos);
+    
+    int opend = time(op_end);
+    int opstart = time(op_start);
     int videolen = time(video_len);
-    int ops = time(op_start);
-    int ope = time(op_end);
+    int cur_time = time(pos);
     
-    
-    for(auto x : commands){
-        if(ps >= ops && ps <= ope) ps = ope;
-        if(x == "next"){
-            
-            if (ps + 10 >= videolen) ps = videolen;
-            else ps += 10;
+    for(int i = 0; i < commands.size(); i++)
+    {
+        cur_time = pass(cur_time, opstart, opend);
+        if(commands[i] == "next")
+        {
+            cur_time = next(cur_time, videolen);
+        
         }
-        else{
-            if(ps - 10 <= 0) ps = 0;
-            else ps -= 10;
-        }
-        if(ps >= ops && ps <= ope) ps = ope;
+        else cur_time = prev(cur_time);
+        cur_time = pass(cur_time, opstart, opend);
+        
     }
-
-    int min = ps / 60;
-    int sec = ps - (60 * min);
-    string strmin = "";
-    string strsec = "";
-    if(min < 10) strmin = "0" + to_string(min);
-    else strmin = to_string(min);
     
-    if(sec < 10) strsec = "0" + to_string(sec);
-    else strsec = to_string(sec);
+    answer = timetostr(cur_time);
     
-    answer = strmin + ":" + strsec;
+    
     return answer;
 
 }
