ac5901b6088ecc16495b274f0f2ea57e6a4226cf
Taegang Yun <73454135+tkthegreat99@users.noreply.github.com>
2024-10-23 16:09:31 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 0 ms, Memory: 3928 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
@@ -1,76 +1,90 @@
 #include <iostream>
+#include <cstring>
 #include <vector>
 #include <algorithm>
 #include <queue>
-#include <map>
 #include <string>
+#include <stack>
+#include <map>
+#include <set>
+#include <list>
+#include <unordered_set>
+#include <unordered_map>
 #include <tuple>
+#include <limits.h>
+#include <math.h>
+#include <deque>
+#include <sstream>
+#define int long long
+#define fastio cin.tie(0), cout.tie(0), ios_base::sync_with_stdio(0)
 
-#define fastio ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0)
 using namespace std;
 
 
-int n,m,sx,sy,ret,x,y;
+int n;
+int mn = 1e9;
+int finish = 0;
+int dp[61][61][61];
 
-int dp[64][64][64], a[3], visited[64][64][64];
-int _a[6][3] = {
+int a[6][3] = {
 	{9, 3, 1},
 	{9, 1, 3},
-	{3, 1, 9},
 	{3, 9, 1},
+	{3, 1, 9},
 	{1, 9, 3},
 	{1, 3, 9}
 };
 
 
-const int dy[] = { -1, 0, 1, 0 };
-const int dx[] = { 0, -1, 0, 1 };
-const int INF = 987654321;
-
-
-
 struct A {
-	int a, b, c;
+	int a;
+	int b;
+	int c;
 };
 
-
-void input();
-void solve(int a, int b, int c);
-
-int main() {
+signed main()
+{
 	fastio;
-	input();
-	solve(a[0], a[1], a[2]);
-	return 0;
 	
-}
-
-void input() {
 	cin >> n;
-	for (int i = 0; i < n; i++) cin >> a[i];
-}
+	int tmp;
+	vector<int> scv;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> tmp;;
+		scv.push_back(tmp);
+	}
+	for (int i = 0; i < 3 - n; i++) scv.push_back(0);
 
-queue <A> q;
-void solve(int a, int b, int c) {
-	visited[a][b][c] = 1;
-	q.push({ a, b, c });
-	while (q.size()) {
-		int a = q.front().a;
-		int b = q.front().b;
-		int c = q.front().c;
+
+
+	queue<A> q;
+	q.push({ scv[0], scv[1], scv[2] });
+	while (q.size())
+	{
+		int scv1 = q.front().a;
+		int scv2 = q.front().b;
+		int scv3 = q.front().c;
+		//cout << scv1 << ", " << scv2 << ", " << scv3 << '\n';
 		q.pop();
-		if (visited[0][0][0]) break;
-		for (int i = 0; i < 6; i++) {
-			int na = max(0, a - _a[i][0]);
-			int nb = max(0, b - _a[i][1]);
-			int nc = max(0, c - _a[i][2]);
-			if (visited[na][nb][nc]) continue;
-			visited[na][nb][nc] = visited[a][b][c] + 1;
-			q.push({ na, nb, nc });
+		for (int i = 0; i < 6; i++)
+		{
+			int next1 = scv1 - a[i][0] <= 0 ? 0 : scv1 - a[i][0];
+			int next2 = scv2 - a[i][1] <= 0 ? 0 : scv2 - a[i][1];
+			int next3 = scv3 - a[i][2] <= 0 ? 0 : scv3 - a[i][2];
+			if (dp[next1][next2][next3] != 0) continue;
+			dp[next1][next2][next3] = dp[scv1][scv2][scv3] + 1;
+			if (next1 == 0 && next2 == 0 && next3 == 0) break;
+			q.push({ next1, next2, next3 });
 		}
 	}
-	cout << visited[0][0][0] - 1 << '\n';
-	
+
+	cout << dp[0][0][0] << '\n';
 }
 
+/*
 
+
+
+
+*/
