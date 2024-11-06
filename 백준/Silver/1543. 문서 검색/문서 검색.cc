994609d27c5f41441e194d7792341dcff489eb72
Taegang Yun <73454135+tkthegreat99@users.noreply.github.com>
2024-11-06 11:21:06 +0900

    [Silver V] Title: 문서 검색, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1543. 문서 검색/문서 검색.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <string>
+#include <stack>
+#include <map>
+#include <set>
+#include <list>
+#include <unordered_set>
+#include <unordered_map>
+#include <tuple>
+#include <limits.h>
+#include <math.h>
+#include <deque>
+#include <sstream>
+#define int long long
+#define fastio cin.tie(0), cout.tie(0), ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+signed main()
+{
+    fastio;
+    int answer = 0;
+    string str;
+
+    string target;
+
+    getline(cin, str);
+    getline(cin, target);
+    int len = target.length();
+
+    if (str.length() < target.length())
+    {
+        cout << 0;
+        return 0;
+    }
+
+    int k;
+    while (( k = str.find(target)) != string::npos)
+    {
+        //str.erase(k, k + len);
+        str = str.substr(k + len);
+        answer++;
+        
+    }
+    cout << answer << '\n';
+}
